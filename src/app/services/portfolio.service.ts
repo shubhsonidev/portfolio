d5c21436c2e590bf3bdf85d30efc268f20e4a737
“Shubh <shubhsoni.dev@gmail.com>
2024-03-12 17:23:19 +0530

    updates

--- src/app/services/portfolio.service.ts
+++ src/app/services/portfolio.service.ts
@@ -1,85 +1,111 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PortfolioService {
-portfolioitems: any = [
-  
-  {
-    mainImage: "https://shubhsonidev.github.io/portfolio/assets/img/portfolio/byajbook_webapp.png",
-    backgroundImage: "https://shubhsonidev.github.io/portfolio/assets/img/portfolio/byajbook_00.png",
-    // gitLink: 'https://github.com/christian-hansen/join-app',
-    // liveLink: 'https://christian-hansen.developerakademie.net/join/',
-    title: 'Byajbook Webapp',
-    // description: '"Byajbook" is a software which manages informal loans.',
-    tags: ['Angular','Typescript', 'HTML', 'CSS']
-  },
-  {
-    mainImage: "https://shubhsonidev.github.io/portfolio/assets/img/portfolio/fivefilter_website.png",
-    backgroundImage: "https://shubhsonidev.github.io/portfolio/assets/img/portfolio/fivefilter_00.png",
-    // gitLink: 'https://github.com/christian-hansen/el-pollo-loco',
-    // liveLink: 'https://christian-hansen.developerakademie.net/elpolloloco/',
-    title: 'FiveFiter eSports Website',
-    // description: '"FiveFiter eSports" is an Indian esports performance brand, laser-focused on seeking out, leveling up, and amplifying gamers and creators.',
-    tags: ['Angular', 'HTML', 'CSS']
-  },
-  {
-    mainImage: "https://shubhsonidev.github.io/portfolio/assets/img/portfolio/byajbook_website.png",
-    backgroundImage: "https://shubhsonidev.github.io/portfolio/assets/img/portfolio/byajbook_01.png",
-    // gitLink: 'https://github.com/christian-hansen/portfolio-website',
-    // liveLink: 'https://www.christian-hansen.dev',
-    title: 'Byajbook Website',
-    // description: '"Byajbook" is a software which manages informal loans.',
-    tags: ['Angular', 'Typescript', 'HTML', 'CSS']
-  },
+  portfolioitems: any = [
     {
-    mainImage: "https://shubhsonidev.github.io/portfolio/assets/img/portfolio/fivefilter_webapp.png",
-    backgroundImage: "https://shubhsonidev.github.io/portfolio/assets/img/portfolio/fivefilter_01.png",
-    // gitLink: 'https://github.com/christian-hansen/ng-weather',
-    // liveLink: 'https://weather.christian-hansen.dev/',
-    title: 'eSports Tournament manager',
-    // description: 'This is Discord based SAAS manager which helps eSports in maintaining and organising Tournaments with realtime-results and smooth conduction of tournament.',
-    tags: ['Angular', 'Typescript', 'API', 'Discord Bots']
-  },
+      mainImage:
+        'https://shubhsonidev.github.io/portfolio/assets/img/portfolio/byajbook_webapp.png',
+      backgroundImage:
+        'https://shubhsonidev.github.io/portfolio/assets/img/portfolio/byajbook_00.png',
+      // gitLink: 'https://github.com/christian-hansen/join-app',
+      // liveLink: 'https://christian-hansen.developerakademie.net/join/',
+      title: 'Byajbook Webapp',
+      // description: '"Byajbook" is a software which manages informal loans.',
+      tags: ['Angular', 'Typescript', 'HTML', 'CSS'],
+    },
     {
-    mainImage: "https://shubhsonidev.github.io/portfolio/assets/img/portfolio/mahi925_website.png",
-    backgroundImage: "https://shubhsonidev.github.io/portfolio/assets/img/portfolio/mahi925_00.png",
-    // gitLink: 'https://github.com/christian-hansen/ng-weather',
-    // liveLink: 'https://weather.christian-hansen.dev/',
-    title: 'Mahi925 Silver Website',
-    // description: '"Mahi925 Silver" is brand which showcases a wide variety of modern and fashionable Silver jewellery',
-    tags: ['Angular', 'HTML', 'CSS']
-  },
+      mainImage:
+        'https://shubhsonidev.github.io/portfolio/assets/img/portfolio/fivefilter_website.png',
+      backgroundImage:
+        'https://shubhsonidev.github.io/portfolio/assets/img/portfolio/fivefilter_00.png',
+      // gitLink: 'https://github.com/christian-hansen/el-pollo-loco',
+      // liveLink: 'https://christian-hansen.developerakademie.net/elpolloloco/',
+      title: 'FiveFiter eSports Website',
+      // description: '"FiveFiter eSports" is an Indian esports performance brand, laser-focused on seeking out, leveling up, and amplifying gamers and creators.',
+      tags: ['Angular', 'HTML', 'CSS'],
+    },
     {
-    mainImage: "https://shubhsonidev.github.io/portfolio/assets/img/portfolio/portfolio.png",
-    backgroundImage: "https://shubhsonidev.github.io/portfolio/assets/img/portfolio/port.png",
-    // gitLink: 'https://github.com/christian-hansen/ng-weather',
-    // liveLink: 'https://weather.christian-hansen.dev/',
-    title: 'Portfolio',
-    // description: '',
-    tags: ['Angular', 'Typescript', 'API', 'HTML', 'CSS']
-  },
+      mainImage:
+        'https://shubhsonidev.github.io/portfolio/assets/img/portfolio/byajbook_website.png',
+      backgroundImage:
+        'https://shubhsonidev.github.io/portfolio/assets/img/portfolio/byajbook_01.png',
+      // gitLink: 'https://github.com/christian-hansen/portfolio-website',
+      // liveLink: 'https://www.christian-hansen.dev',
+      title: 'Byajbook Website',
+      // description: '"Byajbook" is a software which manages informal loans.',
+      tags: ['Angular', 'Typescript', 'HTML', 'CSS'],
+    },
     {
-    mainImage: "https://shubhsonidev.github.io/portfolio/assets/img/portfolio/ratechecker.png",
-    backgroundImage: "https://shubhsonidev.github.io/portfolio/assets/img/portfolio/rate.png",
-    // gitLink: 'https://github.com/christian-hansen/ng-weather',
-    // liveLink: 'https://weather.christian-hansen.dev/',
-    title: 'Gold & Silver Rates Webapp',
-    // description: 'This Webapp shows Real-Time rates of Gold and Silver. It is purely made for retailers for increasing Customer Relations.',
-    tags: [ 'Javascript', 'API', 'HTML5', 'CSS']
-  },
+      mainImage:
+        'https://shubhsonidev.github.io/portfolio/assets/img/portfolio/fivefilter_webapp.png',
+      backgroundImage:
+        'https://shubhsonidev.github.io/portfolio/assets/img/portfolio/fivefilter_01.png',
+      // gitLink: 'https://github.com/christian-hansen/ng-weather',
+      // liveLink: 'https://weather.christian-hansen.dev/',
+      title: 'eSports Tournament manager',
+      // description: 'This is Discord based SAAS manager which helps eSports in maintaining and organising Tournaments with realtime-results and smooth conduction of tournament.',
+      tags: ['Angular', 'Typescript', 'API', 'Discord Bots'],
+    },
     {
-    mainImage: "https://shubhsonidev.github.io/portfolio/assets/img/portfolio/scheme.png",
-    backgroundImage: "https://shubhsonidev.github.io/portfolio/assets/img/portfolio/scheme_00.png",
-    // gitLink: 'https://github.com/christian-hansen/ng-weather',
-    // liveLink: 'https://weather.christian-hansen.dev/',
-    title: '11+1 Gold scheme',
-    // description: 'This Webapp maintains 11+1 scheme which is widely used by gold and silver retailers.',
-    tags: [ 'Javascript','Appscript', 'API', 'HTML5', 'CSS']
-  },
-]
-  constructor() { }
+      mainImage:
+        'https://shubhsonidev.github.io/portfolio/assets/img/portfolio/mahi925_website.png',
+      backgroundImage:
+        'https://shubhsonidev.github.io/portfolio/assets/img/portfolio/mahi925_00.png',
+      // gitLink: 'https://github.com/christian-hansen/ng-weather',
+      // liveLink: 'https://weather.christian-hansen.dev/',
+      title: 'Mahi925 Silver Website',
+      // description: '"Mahi925 Silver" is brand which showcases a wide variety of modern and fashionable Silver jewellery',
+      tags: ['Angular', 'HTML', 'CSS'],
+    },
+    {
+      mainImage:
+        'https://shubhsonidev.github.io/portfolio/assets/img/portfolio/portfolio.png',
+      backgroundImage:
+        'https://shubhsonidev.github.io/portfolio/assets/img/portfolio/port.png',
+      // gitLink: 'https://github.com/christian-hansen/ng-weather',
+      // liveLink: 'https://weather.christian-hansen.dev/',
+      title: 'Portfolio',
+      // description: '',
+      tags: ['Angular', 'Typescript', 'API', 'HTML', 'CSS'],
+    },
+    {
+      mainImage:
+        'https://shubhsonidev.github.io/portfolio/assets/img/portfolio/ratechecker.png',
+      backgroundImage:
+        'https://shubhsonidev.github.io/portfolio/assets/img/portfolio/rate.png',
+      // gitLink: 'https://github.com/christian-hansen/ng-weather',
+      // liveLink: 'https://weather.christian-hansen.dev/',
+      title: 'Gold & Silver Rates Webapp',
+      // description: 'This Webapp shows Real-Time rates of Gold and Silver. It is purely made for retailers for increasing Customer Relations.',
+      tags: ['Javascript', 'API', 'HTML5', 'CSS'],
+    },
+    {
+      mainImage:
+        'https://shubhsonidev.github.io/portfolio/assets/img/portfolio/scheme.png',
+      backgroundImage:
+        'https://shubhsonidev.github.io/portfolio/assets/img/portfolio/scheme_00.png',
+      // gitLink: 'https://github.com/christian-hansen/ng-weather',
+      // liveLink: 'https://weather.christian-hansen.dev/',
+      title: '11+1 Gold scheme Management',
+      // description: 'This Webapp maintains 11+1 scheme which is widely used by gold and silver retailers.',
+      tags: ['Javascript', 'Appscript', 'API', 'HTML5', 'CSS'],
+    },
+    {
+      mainImage:
+        'https://shubhsonidev.github.io/portfolio/assets/img/portfolio/tech.png',
+      backgroundImage:
+        'https://shubhsonidev.github.io/portfolio/assets/img/portfolio/tech1.png',
+      // gitLink: 'https://github.com/christian-hansen/ng-weather',
+      // liveLink: 'https://weather.christian-hansen.dev/',
+      title: 'TechShorts',
+      // description: 'This Webapp maintains 11+1 scheme which is widely used by gold and silver retailers.',
+      tags: ['Javascript', 'React', 'API', 'HTML5', 'CSS'],
+    },
+  ];
+  constructor() {}
 
   getPortfolio() {
     return this.portfolioitems;
--- src/app/services/skills.service.ts
+++ src/app/services/skills.service.ts
@@ -1,30 +1,62 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SkillsService {
   skills: any = [
-    {logo: 'https://shubhsonidev.github.io/portfolio/assets/img/icons/angular-big.png',description: 'Angular'},
-    {logo: 'https://shubhsonidev.github.io/portfolio/assets/img/icons/typescript-big.png',description: 'Typescript'},
-    {logo: 'https://shubhsonidev.github.io/portfolio/assets/img/icons/javascript-big.png',description: 'Javascript'},
-    {logo: 'https://shubhsonidev.github.io/portfolio/assets/img/icons/html5-big.png',description: 'HTML5'},
-    {logo: 'https://shubhsonidev.github.io/portfolio/assets/img/icons/api-big.png',description: 'API'},
-    {logo: 'https://shubhsonidev.github.io/portfolio/assets/img/icons/css-big.png',description: 'CSS'},
-    {logo: 'https://shubhsonidev.github.io/portfolio/assets/img/icons/material-design-big.png',description: 'Material Design'},
-    {logo: 'https://shubhsonidev.github.io/portfolio/assets/img/icons/bootstrap.svg',description: 'Bootstrap'},
-    {logo: 'https://shubhsonidev.github.io/portfolio/assets/img/icons/git.png',description: 'GIT'},
-    {logo: 'https://shubhsonidev.github.io/portfolio/assets/img/icons/flu.png',description: 'Flutter'},
-    {logo: 'https://shubhsonidev.github.io/portfolio/assets/img/icons/appscript.png',description: 'App Script'},
-    {logo: 'https://shubhsonidev.github.io/portfolio/assets/img/icons/c++.png',description: 'C++'},
-    
+    {
+      logo: 'https://shubhsonidev.github.io/portfolio/assets/img/icons/angular-big.png',
+      description: 'Angular',
+    },
+    {
+      logo: 'https://shubhsonidev.github.io/portfolio/assets/img/icons/react.png',
+      description: 'React',
+    },
+    {
+      logo: 'https://shubhsonidev.github.io/portfolio/assets/img/icons/typescript-big.png',
+      description: 'Typescript',
+    },
+    {
+      logo: 'https://shubhsonidev.github.io/portfolio/assets/img/icons/javascript-big.png',
+      description: 'Javascript',
+    },
+    {
+      logo: 'https://shubhsonidev.github.io/portfolio/assets/img/icons/html5-big.png',
+      description: 'HTML5',
+    },
+    {
+      logo: 'https://shubhsonidev.github.io/portfolio/assets/img/icons/api-big.png',
+      description: 'API',
+    },
+    {
+      logo: 'https://shubhsonidev.github.io/portfolio/assets/img/icons/css-big.png',
+      description: 'CSS',
+    },
+    {
+      logo: 'https://shubhsonidev.github.io/portfolio/assets/img/icons/material-design-big.png',
+      description: 'Material Design',
+    },
+    // {logo: 'https://shubhsonidev.github.io/portfolio/assets/img/icons/bootstrap.svg',description: 'Bootstrap'},
+    {
+      logo: 'https://shubhsonidev.github.io/portfolio/assets/img/icons/git.png',
+      description: 'GIT',
+    },
+    {
+      logo: 'https://shubhsonidev.github.io/portfolio/assets/img/icons/flu.png',
+      description: 'Flutter',
+    },
+    {
+      logo: 'https://shubhsonidev.github.io/portfolio/assets/img/icons/appscript.png',
+      description: 'App Script',
+    },
+    {
+      logo: 'https://shubhsonidev.github.io/portfolio/assets/img/icons/c++.png',
+      description: 'C++',
+    },
+  ];
 
-
-  ]
-
-  constructor() { }
-
-  
+  constructor() {}
 
   getSkills() {
     return this.skills;

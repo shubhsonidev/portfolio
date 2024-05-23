3f4704a7f93754d5d491413907fbfe9592c8998d
“Shubh <shubhsoni.dev@gmail.com>
2024-05-23 18:49:57 +0530

    fix: whatsapp api fix

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,22 +1,20 @@
 import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
+import {
+  HTTP_INTERCEPTORS,
+  HttpClient,
+  HttpClientModule,
+} from '@angular/common/http';
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.scss'],
 })
 export class ContactFormComponent {
-  constructor(
-
-    private http: HttpClient,
-
-
-  ) {}
+  constructor(private http: HttpClient) {}
   @ViewChild('contactForm') formRef!: NgForm;
   inputData = { inputName: '', inputEmail: '', inputMessage: '' };
-showSuccess: boolean = false;
-
+  showSuccess: boolean = false;
 
   appendFormDatas(
     formData: FormData,
@@ -38,25 +36,33 @@ showSuccess: boolean = false;
   async sendMail() {
     //Animation anzeigen
     console.log(
-      'Message to send:','\n',
-      this.inputData.inputName,'\n',
-      this.inputData.inputEmail,'\n',
+      'Message to send:',
+      '\n',
+      this.inputData.inputName,
+      '\n',
+      this.inputData.inputEmail,
+      '\n',
       this.inputData.inputMessage
     );
     //senden
     this.http
-    .get<any>(
-      'https://script.google.com/macros/s/AKfycbxXC-BrkjNB3G86kGqVw04veZa1F5St40T89PgmclU7UrFu62vK70JhvnvWF0rCjPLE/exec?name='+this.inputData.inputName +'&email='+this.inputData.inputEmail+'&message=' + this.inputData.inputMessage,
-    )
-    .subscribe((res) => {
-      this.showSuccess = true;
+      .get<any>(
+        'https://script.google.com/macros/s/AKfycbzQGDyTuq9-69zTTkSwIo-k9M_Zhy_Afa9RjrGLm_27Vwt5wEktMcD_5RhsR49-o25c/exec?name=' +
+          this.inputData.inputName +
+          '&email=' +
+          this.inputData.inputEmail +
+          '&message=' +
+          this.inputData.inputMessage
+      )
+      .subscribe((res) => {
+        this.showSuccess = true;
 
-      //TODO 
-      setTimeout(() => {
-        this.showSuccess = false
-      }, 5000);
-    });
-  
+        //TODO
+        setTimeout(() => {
+          this.showSuccess = false;
+        }, 5000);
+      });
+
     //Reset
     this.formRef.resetForm();
     //Text anzeigen "Nachricht gesendet"
